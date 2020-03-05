3d0cf04d1797efbe33a225279e024cfee2a44475
vikylenaidoo <vikylenaidoo@gmail.com>
2020-03-05 16:02:15 +0200

    Prac2

--- Prac1/timing_execution.m
+++ Prac1/timing_execution.m
@@ -1,6 +1,6 @@
 
 
-tic; white = rand(48000*10, 1)*2 - 1; runtime = toc();
+tic; white = rand(8000*10, 1)*2 - 1; runtime = toc();
 disp(strcat("It took: ", num2str(runtime*1000), " ms to run"));
 
 tic; white = createwhiten(10);runtime = toc();
